48d9260ad6e8f78ecc112e1dc3661543fc8d0d36
Haakon Wiland <haakonwiland@gmail.com>
2025-06-28 13:46:12 +0200

    some small additions

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,22 @@
 from blackjackclasses import Card, CardDeck, Player
 
-## Main game loop ## 
+## Main game loop ##
+def blackjackGame():
+     
+    deck = CardDeck() 
+    print(deck.getDeck())
+
+    card = deck.drawCard()
+    print(f"card : {card}")
+    
+    # dealer = Player(deck)
+    # dealer.newCard 
+    # print(f"Dealer hand: {dealer.hand}")
+
+
+
+blackjackGame()
+
+
+
+
--- blackjackclasses.py
+++ blackjackclasses.py
@@ -7,7 +7,12 @@ class Card:
     def __init__(self, Value, Suit):
         self.value = Value
         self.suit = Suit
+    
+    def __str__(self):
+        return f"{self.value} of {self.suit}"
 
+    def __repr__(self):
+        return self.__str__()
 
 # TODO: We want value and suit to only have "value" and "suit" as possible values
 
@@ -15,40 +20,36 @@ class Card:
 class CardDeck:
     def __init__(self):
 
-        cardDeck: list[tuple] = []
+        self.cardDeck: list[tuple] = []
 
         for v in possibleValues:
             for s in possibleSuits:
-                card: tuple = (v, s)
-                cardDeck.append(card)
+                card: tuple = Card(v, s)
+                self.cardDeck.append(card)
 
-        self.deck = cardDeck
         self.shuffle()
 
-        ## Debug functions ##
-        self.length = len(cardDeck)
+    def getDeck(self):
+        return self.cardDeck
+
 
     # TODO: This has do shuffle good, find optimal way later.
     def shuffle(self):
         randomValue = 0
-        # shuffledDeck = []
+        
 
-        for card in range(len(self.deck)):
+        for card in range(len(self.cardDeck)):
 
-            ## This is wrong, get decks with 5 11's
             randomValue: int = random.randint(0, 10000)
             index: int = (randomValue) % 43
                 
-            self.deck[card], self.deck[index] = self.deck[index], self.deck[card]
+            self.cardDeck[card] = self.cardDeck[index]
+            self.cardDeck[index] = self.cardDeck[card]
 
-            # shuffledDeck.append(self.deck[index])
-            ##
-
-        # self.deck = shuffledDeck
 
     def drawCard(self):
-        card = self.deck[0]
-        self.deck = self.deck[1:]
+        card = self.cardDeck[0]
+        self.cardDeck = self.cardDeck[1:] 
 
         return card
 
@@ -56,18 +57,28 @@ class CardDeck:
 class Player:
     def __init__(self, deck):
         self.hand: list = []
-        self.hand.append(deck.drawCard())
+        self.sum = 0
+
+    def newCard(self, deck):
+        card = deck.getDeck[0]
+        self.hand.append(card)
+        
+
+
+        # self.hand.append(deck.drawCard)
         # The dealer give cards in between 
         # self.hand.append(deck.drawCard())
 
-        self.sum = 0 
-        for card in self.hand:
-           self.sum += card[0]
+        # self.sum = 0 
+        # for card in self.hand:
+        #     self.sum += card
+
+
 
 ## Testing ##        
 # deck = CardDeck()
 # p = Player(deck)
-#
+
 #
 # print(f"Deck: {deck.deck} \n\n Player card: {p.hand} \n\n Player sum: {p.sum}")
 #
